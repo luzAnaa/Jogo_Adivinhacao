14fd77cbcc4ab48030376704198f0da4643f2ed5
luzAnaa <142361084+luzAnaa@users.noreply.github.com>
2025-09-03 16:17:18 -0300

    Java
    Sem css

---
+++ js/adivinhação.js
@@ -0,0 +1,27 @@
+var sorteado, inserido
+
+sorteado = Math.round((Math.random() * 100)+1)
+
+inserido = parseInt(prompt("Adivinhe o número:"))
+
+if (inserido == sorteado) {
+    alert("Parabéns! Você acertou!")
+}
+
+else if (inserido != sorteado) {
+
+    while (inserido != sorteado) {
+        if (inserido < sorteado) {
+            inserido = prompt("Tente um número maior:")
+
+        }
+        else if (inserido > sorteado) {
+            inserido = prompt("Tente um número menor:")
+        }
+    }
+}
+
+
+else if (inserido == sorteado) {
+    alert("Parabéns! Você acertou!")
+}
