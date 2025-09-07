423991cf036d91c1912798151d68780e74a7a1bb
Analubk <ana.breginski.ifpr.2023@gmail.com>
2025-09-06 22:32:15 -0300

    maior de 'numero'

--- js/adivinhação.js
+++ js/adivinhação.js
@@ -2,7 +2,7 @@ var sorteado, inserido
 
 sorteado = Math.round((Math.random() * 100)+1)
 
-inserido = parseInt(prompt("Adivinhe o número:"))
+inserido = parseInt(prompt("Adivinhe o número de 1 a 100:"))
 
 if (inserido == sorteado) {
     alert("Parabéns! Você acertou!")
@@ -12,11 +12,11 @@ else if (inserido != sorteado) {
 
     while (inserido != sorteado) {
         if (inserido < sorteado) {
-            inserido = prompt("Tente um número maior:")
+            inserido = prompt("Tente um número maior que " + inserido)
 
         }
         else if (inserido > sorteado) {
-            inserido = prompt("Tente um número menor:")
+            inserido = prompt("Tente um número menor que " + inserido)
         }
     }
     if (inserido == sorteado) {
